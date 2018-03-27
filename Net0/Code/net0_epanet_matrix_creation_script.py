d3db4a1e0022beae79348f73a5e240a1741d4f7c
Praveen Venkateswaran <vpraveen99@gmail.com>
2018-03-26 21:18:24 -0700

    fixed epanet executable relative working directory path

--- Net0/Code/net0_epanet_matrix_creation_script.py
+++ Net0/Code/net0_epanet_matrix_creation_script.py
@@ -42,6 +42,7 @@ final_distance_matrix = list()
 final_demand_shortage_matrix = list()
 final_detection_capability_matrix = list()
 final_mobile_deployment_matrix = list()
+final_mobile_deployment_time_matrix = list()
 
 # print len(unique_node_list)
 
@@ -68,19 +69,21 @@ for i in xrange(0,len(unique_node_list)):
 
 	distance_array,demand_shortage_array,detection_time_array,detection_capability_array = leakMatrixCreation(unique_node_list[i],unique_node_id,isConnected_list,connected_distance,connected_velocity,connected_time,node_first_time_store,node_second_time_store)
 
-	mobile_traversal_array = mobileMatrixCreation(unique_node_list[i],unique_node_list,isConnected_list)
+	mobile_traversal_array,mobile_traversal_time_array = mobileMatrixCreation(unique_node_list[i],unique_node_list,isConnected_list,connected_time)
 
 	final_detection_time_matrix.append(detection_time_array)
 	final_distance_matrix.append(distance_array)
 	final_demand_shortage_matrix.append(demand_shortage_array)
 	final_detection_capability_matrix.append(detection_capability_array)
 	final_mobile_deployment_matrix.append(mobile_traversal_array)
+	final_mobile_deployment_time_matrix.append(mobile_traversal_time_array)
 
 final_detection_time_matrix = np.asarray(final_detection_time_matrix).T
 final_distance_matrix = np.asarray(final_distance_matrix).T
 final_demand_shortage_matrix = np.asarray(final_demand_shortage_matrix).T
 final_detection_capability_matrix = np.asarray(final_detection_capability_matrix).T
 final_mobile_deployment_matrix = np.asarray(final_mobile_deployment_matrix).T
+final_mobile_deployment_time_matrix = np.asarray(final_mobile_deployment_time_matrix).T
 
 
 np.savetxt("../Output/detectionTime.csv", final_detection_time_matrix, delimiter=",")
@@ -88,7 +91,7 @@ np.savetxt("../Output/distance.csv", final_distance_matrix, delimiter=",")
 np.savetxt("../Output/demandShortage.csv", final_demand_shortage_matrix, delimiter=",")
 np.savetxt("../Output/detectionCapability.csv", final_detection_capability_matrix, delimiter=",")
 np.savetxt("../Output/mobileDeployment.csv",final_mobile_deployment_matrix,delimiter=",")
-
+np.savetxt("../Output/mobileDeploymentTime.csv",final_mobile_deployment_time_matrix,delimiter=",")
 
 # print final_detection_time_matrix
 # print final_detection_capability_matrix
@@ -101,9 +104,35 @@ np.savetxt("../Output/mobileDeployment.csv",final_mobile_deployment_matrix,delim
 # In each iteration we compute the traversal probability matrix T as a sum of the matrices. 
 
 M = final_mobile_deployment_matrix
-for i in xrange(0,10):
-	print(M)
-	# M = np.dot(M,final_mobile_deployment_matrix)
-	M = np.square(M)
+M_time = final_mobile_deployment_time_matrix
 
-# print(np.dot(final_mobile_deployment_matrix,final_mobile_deployment_matrix))
+final_traversal_probability_matrix = final_mobile_deployment_matrix
+final_traversal_time_matrix = final_mobile_deployment_time_matrix
+
+while len(M[np.nonzero(M)]) > 0:			 # There exist non-zero elements
+	print(M) 
+	M = np.dot(M,final_mobile_deployment_matrix)	
+	# M_time = np.dot(M_time,final_mobile_deployment_time_matrix)
+
+	final_traversal_probability_matrix = final_traversal_probability_matrix + M
+
+
+# print(final_traversal_probability_matrix)
+# print(M_time)
+np.savetxt("../Output/traversalProbability.csv",final_traversal_probability_matrix,delimiter=",")
+
+
+# Now given traversal probability matrix, we compute the minimum number of sensors required to achieve the required coverage probability
+
+REQUIRED_COVERAGE_PROBABILITY = 0.7
+traversal_capability_matrix = final_traversal_probability_matrix
+for i in xrange(0,len(unique_node_list)):
+	for j in xrange(0,len(unique_node_list)):
+		if final_traversal_probability_matrix[i][j] == 0:
+			traversal_capability_matrix[i][j] = 0
+		else:
+			number_of_sensors = np.log(1 - REQUIRED_COVERAGE_PROBABILITY) / np.log(1 - final_traversal_probability_matrix[i][j])
+			traversal_capability_matrix[i][j] = max(final_traversal_probability_matrix[i][j],math.ceil(number_of_sensors))
+
+# print(traversal_capability_matrix)
+np.savetxt("../Output/traversalCapability.csv",traversal_capability_matrix,delimiter=",")
